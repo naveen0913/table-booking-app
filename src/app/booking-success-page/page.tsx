1fcf9776561c9e462df17eb7aefcd5046809817c
U Naveen Babu <136116172+naveen0913@users.noreply.github.com>
2025-01-06 16:49:33 +0530

    Update page.tsx

--- src/app/booking-success-page/page.tsx
+++ src/app/booking-success-page/page.tsx
@@ -1,22 +1,60 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
+
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import styles from './booking-success.module.css';
 
-const SuccessPage = () => {
+interface BookingData {
+  // bookingId: string;
+  // userId: string;
+  // tableId: string;
+  date: string;
+  time: string;
+  guests: number;
+  name: string;
+  contact: string;
+  updatedAt: string;
+  createdAt: string;
+}
 
-    const router = useRouter();
+const SuccessPage = () => {
+  const router = useRouter();
+  const [bookingData, setBookingData] = useState<BookingData | null>(null);
+
+  useEffect(() => {
+    const data = sessionStorage.getItem('bookingData');
+    if (data) {
+      setBookingData(JSON.parse(data));
+    }
+  }, []);
 
   return (
     <div className={styles.main}>
-      <img className={styles.img} src="/success-logo.png" alt="Booking Success"/>
+      <img className={styles.img} src="/success-logo.png" alt="Booking Success" />
       <h3 className={styles.heading}>Table Booking Successful</h3>
+
+      {bookingData ? (
+        <div className={styles.details}>
+          {/* <p><strong>Booking ID:</strong> {bookingData.bookingId}</p> */}
+          <p><strong>Name : </strong> {bookingData.name}</p>
+          <p><strong>Contact : </strong> {bookingData.contact}</p>
+          <p><strong>Date : </strong> {bookingData.date}</p>
+          <p><strong>Time : </strong> {bookingData.time}</p>
+          <p><strong>Guests : </strong> {bookingData.guests}</p>
+          {/* <p><strong>Table ID:</strong> {bookingData.tableId}</p> */}
+        </div>
+      ) : (
+        <p>Loading booking details...</p>
+      )}
+
       <div className={styles.div}>
-        <button onClick={()=>router.push('/bookings')} className={styles.button}>My Bookings</button>
-        <button onClick={()=>router.push('/')} className={styles.button}>Home Page</button>
+        <button onClick={() => router.push('/bookings')} className={styles.booking}>My Bookings</button>
+        <button onClick={() => router.push('/')} className={styles.button}>Home Page</button>
       </div>
     </div>
   );
 };
 
 export default SuccessPage;
+
